975980a65575d3071b1658e51873278f20ab16c5
wlissis <wlissisdev@gmail.com>
2024-03-11 07:42:32 -0300

    adicona o campo image_url na tabela

--- src/controllers/createProductController.js
+++ src/controllers/createProductController.js
@@ -2,15 +2,15 @@ const { db } = require("../database/connect");
 
 async function createProduct(request, response) {
     try {
-        const { name, description, value } = request.body;
+        const { name, description, img_url, value } = request.body;
 
-        if(name == null || description == null || value == null){
+        if (name == null || description == null || value == null || img_url == null) {
             return response.status(401).send("invalid request value");
         }
-       
 
-        const query = "INSERT INTO products(name, description, value) VALUES($1,$2,$3);"
-        await db.query(query, [name, description, value]);
+
+        const query = "INSERT INTO products(name, description, value,  img_url) VALUES($1,$2,$3,$4);"
+        await db.query(query, [name, description, value, img_url]);
         return response.status(201).send("created")
     } catch (error) {
         console.error(error)
--- src/controllers/updateProductController.js
+++ src/controllers/updateProductController.js
@@ -3,7 +3,7 @@ const { db } = require("../database/connect");
 async function updateProduct(request, response) {
     try {
         const { id } = request.params
-        const { name, description, value } = request.body;
+        const { name, description, value, img_url } = request.body;
 
         const existedProduct = (await db.query("SELECT * FROM products WHERE id=$1",[id])).rows;
 
@@ -16,9 +16,9 @@ async function updateProduct(request, response) {
             return response.status(401).send("invalid request value");
         }
         
-        const query = " UPDATE products SET name = $1, description = $2, value = $3 WHERE id = $4;"
+        const query = " UPDATE products SET name = $1, description = $2, value = $3  img_url=$4 WHERE id = $5;"
         
-        const result = (await db.query(query, [name, description, value, id])).command
+        const result = (await db.query(query, [name, description, value, img_url,id])).command
 
         return response.status(200).json(result);
     } catch (error) {
