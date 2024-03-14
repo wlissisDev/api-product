cce0852f2565a59d961cf53120a513e0767b1136
Wlissis Ribeiro <58451362+wlissisDev@users.noreply.github.com>
2024-03-14 19:23:01 -0300

    Update connect.js

--- src/database/connect.js
+++ src/database/connect.js
@@ -4,11 +4,16 @@ const connectionString = process.env.DATABASE_URL;
 const db = new pg.Client({connectionString});
 db.connect();
 
-const tableCreate = "CREATE TABLE products (    id SERIAL PRIMARY KEY,    name VARCHAR(255), img_url VARCHAR(255),   description VARCHAR(255),    value INT);"
+const tableCreate = "CREATE TABLE IF NOT EXISTS products (    id SERIAL PRIMARY KEY,    name VARCHAR(255), img_url VARCHAR(255),   description VARCHAR(255),    value INT);"
 
 db.query(tableCreate, [], (err, result) => {
     if (err) console.log("Error: " + err)
 });
+
+db.query("ALTER TABLE products ADD COLUMN img_url VARCHAR(255);", [], (err, result) => {
+    if (err) console.log("Error: " + err)
+});
+
 
 
 module.exports={db}
